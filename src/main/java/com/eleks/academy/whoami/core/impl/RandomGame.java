fa6d9f26e5ccd2ba389f9f589bea0ccb7f2ad3b7
Aleksey Vasilenko <vasilenko.aleksey.v@gmail.com>
2022-05-05 15:23:27 +0300

    [LECHW-7] Added custom runtime exception implementation in RandomGame class makeTurn and assignCharacters methods

--- src/main/java/com/eleks/academy/whoami/core/impl/RandomGame.java
+++ src/main/java/com/eleks/academy/whoami/core/impl/RandomGame.java
@@ -1,9 +1,6 @@
 package com.eleks.academy.whoami.core.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -14,25 +11,30 @@ import java.util.stream.Collectors;
 import com.eleks.academy.whoami.core.Game;
 import com.eleks.academy.whoami.core.Player;
 import com.eleks.academy.whoami.core.Turn;
+import com.eleks.academy.whoami.exceptions.FailToObtainException;
 
 public class RandomGame implements Game {
 
-	private static final int DURATION = 2;
+	private static final int DURATION = 1;
 	private static final TimeUnit UNIT = TimeUnit.MINUTES;
 
 	private Map<String, String> playersCharacter = new ConcurrentHashMap<>();
 	private final List<Player> players;
 	private final List<String> availableCharacters;
 	private Turn currentTurn;
+	private final Map<Player, String> playerNames = new HashMap<>();
 
-	
 	private final static String YES = "Yes";
 	private final static String NO = "No";
-	
-	public RandomGame(List<Player> players, List<String> availableCharacters) { 
+
+	public RandomGame(List<Player> players, List<String> availableCharacters) {
 		this.availableCharacters = new ArrayList<String>(availableCharacters);
 		this.players = new ArrayList<>(players.size());
 		players.forEach(this::addPlayer);
+	}
+
+	public List<Player> getPlayers() {
+		return players;
 	}
 
 	private void addPlayer(Player player) {
@@ -43,7 +45,6 @@ public class RandomGame implements Game {
 			this.players.add(player);
 			this.availableCharacters.add(character);
 		} catch (InterruptedException | ExecutionException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (TimeoutException e) {
 			System.err.printf("Player did not suggest a character within %d %s%n", DURATION, UNIT);
@@ -58,9 +59,9 @@ public class RandomGame implements Game {
 		try {
 			guessersName = currentGuesser.getName().get(DURATION, UNIT);
 		} catch (InterruptedException | ExecutionException | TimeoutException e) {
-			// TODO: Add custom runtime exception implementation
-			throw new RuntimeException("Failed to obtain a player's name", e);
+			throw new FailToObtainException("Failed to obtain a player's name", e);
 		}
+		playerNames.put(currentGuesser, guessersName);
 		String guesserAnswer = String.valueOf(currentGuesser.isReadyForGuess());
 		if (guesserAnswer.equals("Yes")) {
 			String guess = String.valueOf(currentGuesser.getGuess());
@@ -69,14 +70,14 @@ public class RandomGame implements Game {
 					.collect(Collectors.toSet());
 			long positiveCount = answers.stream().filter(a -> YES.equals(String.valueOf(a))).count();
 			long negativeCount = answers.stream().filter(a -> NO.equals(String.valueOf(a))).count();
-			
+
 			boolean win = positiveCount > negativeCount;
-			
+
 			if (win) {
 				players.remove(currentGuesser);
 			}
 			return win;
-			
+
 		} else {
 			String question;
 			try {
@@ -92,7 +93,7 @@ public class RandomGame implements Game {
 			long negativeCount = answers.stream().filter(a -> NO.equals(String.valueOf(a))).count();
 			return positiveCount > negativeCount;
 		}
-		
+
 	}
 
 	private void assignCharacters() {
@@ -102,16 +103,15 @@ public class RandomGame implements Game {
 				return f.get(DURATION, UNIT);
 			} catch (InterruptedException | ExecutionException e) {
 				Thread.currentThread().interrupt();
-				// TODO: Add custom runtime exception implementation
-				throw new RuntimeException("Failed to obtain a player's name", e);
+				throw new FailToObtainException("Failed to obtain a player's name", e);
 			} catch (TimeoutException e) {
 				// TODO: Choose a name from a pool of names, i.e. Anonymous Badger etc.
 				throw new RuntimeException("Player did not provide a name within %d %s".formatted(DURATION, UNIT));
 			}
 		}).forEach(name -> this.playersCharacter.put(name, this.getRandomCharacter()));
-		
+
 	}
-	
+
 	@Override
 	public void initGame() {
 		this.assignCharacters();
@@ -123,7 +123,7 @@ public class RandomGame implements Game {
 	public boolean isFinished() {
 		return players.size() == 1;
 	}
-	
+
 	private String getRandomCharacter() {
 		int randomPos = (int)(Math.random() * this.availableCharacters.size());
 		// TODO: Ensure player never receives own suggested character
@@ -138,7 +138,7 @@ public class RandomGame implements Game {
 	@Override
 	public void play() {
 		boolean gameStatus = true;
-		
+
 		while (gameStatus) {
 			boolean turnResult = this.makeTurn();
 
