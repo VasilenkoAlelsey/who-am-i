5408d043a66e88c19390a17ed7484648420eecf0
Aleksey Vasilenko <vasilenko.aleksey.v@gmail.com>
2022-05-16 18:46:49 +0300

    [LECHW-11] Test to enrollGame and startGame

--- src/test/java/com/eleks/academy/whoami/controller/GameControllerTest.java
+++ src/test/java/com/eleks/academy/whoami/controller/GameControllerTest.java
@@ -29,70 +29,93 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 class GameControllerTest {
 
-	private final GameServiceImpl gameService = mock(GameServiceImpl.class);
-	private final GameController gameController = new GameController(gameService);
-	private final NewGameRequest gameRequest = new NewGameRequest();
-	private MockMvc mockMvc;
+    private final GameServiceImpl gameService = mock(GameServiceImpl.class);
+    private final GameController gameController = new GameController(gameService);
+    private final NewGameRequest gameRequest = new NewGameRequest();
+    private MockMvc mockMvc;
 
-	@BeforeEach
-	public void setMockMvc() {
-		mockMvc = MockMvcBuilders.standaloneSetup(gameController)
-				.setControllerAdvice(new GameControllerAdvice()).build();
-		gameRequest.setMaxPlayers(5);
-	}
+    @BeforeEach
+    public void setMockMvc() {
+        mockMvc = MockMvcBuilders.standaloneSetup(gameController)
+                .setControllerAdvice(new GameControllerAdvice()).build();
+        gameRequest.setMaxPlayers(5);
+    }
 
-	@Test
-	void findAvailableGames() throws Exception {
-		this.mockMvc.perform(
-						MockMvcRequestBuilders.get("/games")
-								.header("X-Player", "player"))
-				.andExpect(status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0]").doesNotHaveJsonPath());
-	}
+    @Test
+    void findAvailableGames() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/games")
+                                .header("X-Player", "player"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]").doesNotHaveJsonPath());
+    }
 
-	@Test
-	void createGame() throws Exception {
-		GameDetails gameDetails = new GameDetails();
-		gameDetails.setId("12613126");
-		gameDetails.setStatus("WaitingForPlayers");
-		when(gameService.createGame(eq("player"), any(NewGameRequest.class))).thenReturn(gameDetails);
-		this.mockMvc.perform(
-						MockMvcRequestBuilders.post("/games")
-								.header("X-Player", "player")
-								.contentType(MediaType.APPLICATION_JSON)
-								.content("{\n" +
-										"    \"maxPlayers\": 2\n" +
-										"}"))
-				.andExpect(status().isCreated())
-				.andExpect(jsonPath("id").value("12613126"))
-				.andExpect(jsonPath("status").value("WaitingForPlayers"));
-	}
+    @Test
+    void createGame() throws Exception {
+        GameDetails gameDetails = new GameDetails();
+        gameDetails.setId("16527053");
+        gameDetails.setStatus("WaitingForPlayers");
+        when(gameService.createGame(eq("player"), any(NewGameRequest.class))).thenReturn(gameDetails);
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/games")
+                                .header("X-Player", "player")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\n" +
+                                        "    \"maxPlayers\": 2\n" +
+                                        "}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").value("16527053"))
+                .andExpect(jsonPath("status").value("WaitingForPlayers"));
+    }
 
-	@Test
-	void createGameFailedWithException() throws Exception {
-		this.mockMvc.perform(
-						MockMvcRequestBuilders.post("/games")
-								.header("X-Player", "player")
-								.contentType(MediaType.APPLICATION_JSON)
-								.content("{\n" +
-										"    \"maxPlayers\": null\n" +
-										"}"))
-				.andExpect(status().isBadRequest())
-				.andExpect(content().string("{\"message\":\"Validation failed!\"," +
-						"\"details\":[\"maxPlayers must not be null\"]}"));
-	}
+    @Test
+    void createGameFailedWithException() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/games")
+                                .header("X-Player", "player")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\n" +
+                                        "    \"maxPlayers\": null\n" +
+                                        "}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("{\"message\":\"Validation failed!\"," +
+                        "\"details\":[\"maxPlayers must not be null\"]}"));
+    }
 
-	@Test
-	void suggestCharacter() throws Exception {
-		doNothing().when(gameService).suggestCharacter(eq("1234"), eq("player"), any(CharacterSuggestion.class));
-		this.mockMvc.perform(
-						MockMvcRequestBuilders.post("/games/1234/characters")
-								.header("X-Player", "player")
-								.contentType(MediaType.APPLICATION_JSON)
-								.content("{\n" +
-										"    \"character\": \" char\"\n" +
-										"}"))
-				.andExpect(status().isOk());
-		verify(gameService, times(1)).suggestCharacter(eq("1234"), eq("player"), any(CharacterSuggestion.class));
-	}
+    @Test
+    void suggestCharacter() throws Exception {
+        doNothing().when(gameService).suggestCharacter(eq("1234"), eq("player"), any(CharacterSuggestion.class));
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/games/1234/characters")
+                                .header("X-Player", "player")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\n" +
+                                        "    \"character\": \" char\"\n" +
+                                        "}"))
+                .andExpect(status().isOk());
+        verify(gameService, times(1)).suggestCharacter(eq("1234"), eq("player"), any(CharacterSuggestion.class));
+    }
+
+    @Test
+    void enrollGame() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/games/1234")
+                                .header("X-Player", "player")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\n" +
+                                        "    \"player\": \" char\"\n" +
+                                        "}"))
+                .andExpect(status().isOk());
+        verify(gameService, times(1)).enrollToGame(eq("1234"), eq("player"));
+    }
+
+    @Test
+    void startGame() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/games/1234")
+                                .header("X-Player", "player"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]").doesNotHaveJsonPath());
+        verify(gameService, times(1)).startGame(eq("1234"), eq("player"));
+    }
 }
